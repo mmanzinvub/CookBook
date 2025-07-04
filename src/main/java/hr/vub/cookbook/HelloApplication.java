ac91db06b1427814583e0e59c1bdba148ea9117f
mmanzinvub <150946687+mmanzinvub@users.noreply.github.com>
2025-07-04 05:22:20 +0200

    Add SceneManager and update application startup
    Introduced a SceneManager class to manage scene registration and switching in the application. Updated HelloApplication to use SceneManager, load the login screen on startup, and register multiple scenes for easier navigation.

--- src/main/java/hr/vub/cookbook/HelloApplication.java
+++ src/main/java/hr/vub/cookbook/HelloApplication.java
@@ -2,22 +2,33 @@ package hr.vub.cookbook;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        System.out.println("Starting CookBook application...");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/hr/vub/cookbook/login-screen.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 640.0, 640.0);
+
+        stage.setTitle("CookBook - Prijava");
         stage.setScene(scene);
         stage.show();
+
+        SceneManager.setStage(stage);
+        SceneManager.registerScene("hello-view", "/hr/vub/cookbook/hello-view.fxml");
+        SceneManager.registerScene("login-screen", "/hr/vub/cookbook/login-screen.fxml");
+        SceneManager.registerScene("recipeTable", "/hr/vub/cookbook/recipe-table.fxml");
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
